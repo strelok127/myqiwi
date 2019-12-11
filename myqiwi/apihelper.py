dfca1ace42616bc229849b3a14a785019d3a3689
daveusa31 <41593484+daveusa31@users.noreply.github.com>
2019-12-11 10:09:17 +0300

    Add error codes
    Добавил неколько ошибок при получение статус кодов от киви

--- myqiwi/apihelper.py
+++ myqiwi/apihelper.py
@@ -13,13 +13,21 @@ def make_request(method_name, headers, method="get", params=None, js=None):
     elif method == "post":
         r = requests.post(url, params=params, headers=headers, json=js)
 
+    if r.status_code == 400:
+        raise ValueError("Query syntax error (incorrect data format)")
 
     if r.status_code == 401:
         raise ValueError("Invalid token")
 
-    elif r.status_code == 402:
+    elif r.status_code == 403:
         raise ValueError("Token does not have enough permissions")
+
+    elif r.status_code == 404:
+        raise ValueError("No transaction found or no payments\
+        with the specified attributes")
         
     else:    
         return json.loads(r.text)
+
+
 
