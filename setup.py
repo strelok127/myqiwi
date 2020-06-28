b14e72088edb36d0b86eabae9ca3cfa4269d0291
daveusa31 <41593484+daveusa31@users.noreply.github.com>
2020-06-28 17:35:09 +0300

    Update installer

--- setup.py
+++ setup.py
@@ -8,20 +8,19 @@ import myqiwi as lib
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-with open("requirements.txt", encoding="utf-8") as r:
+with open("requirements.txt", "r", encoding="utf-8") as r:
     requires = [i.strip() for i in r] # Зависимости
 
-with open("README.md", "r", "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
 
-# Получение данных из файла version
 about = {}
 path = os.path.join(here, "myqiwi", "__version__.py")
 with codecs.open(path, "r", "utf-8") as f:
     exec(f.read(), about)
 
-include = ["requirements.txt", "README.md"]
+
 
 setuptools.setup(
     name=about["__title__"],
@@ -32,7 +31,7 @@ setuptools.setup(
     author=about["__author__"],
     author_email=about["__author_email__"],
     url=about["__url__"],
-    packages=setuptools.find_packages(include=include),
+    packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires=">=3.5",
     install_requires=requires,
