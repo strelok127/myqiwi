6ce802b39c66cb36d722a8d0fd454aec0e6a38c2
daveusa31 <bakteev2003@yandex.ru>
2020-12-01 21:14:58 +0300

    Переписал юнит тесты на pytest

---
+++ tests/test_qiwi.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+
+import myqiwi
+
+token = os.environ.get("QIWI_TOKEN")
+
+if token:
+    qiwi = myqiwi.Wallet(token)
+else:
+    qiwi = None
+
+
+@pytest.mark.skipif(qiwi is None, reason="")
+def test_get_phone():
+    phone = qiwi.phone
+
+    assert isinstance(phone, int)
+
+
+@pytest.mark.skipif(qiwi is None, reason="")
+def test_balance():
+    balance = qiwi.balance()
+
+    assert 0 <= balance
+    assert isinstance(balance, float)
+
+
+@pytest.mark.skipif(qiwi is None, reason="")
+def test_profile():
+    profile = qiwi.profile()
+
+    assert isinstance(profile, dict)
