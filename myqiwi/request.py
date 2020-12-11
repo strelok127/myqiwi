fc6ad7133b4ceaf189a810a5038248b5f1d6f890
daveusa31 <bakteev2003@yandex.ru>
2020-12-11 22:05:05 +0300

    Delete warnings from request function

--- myqiwi/request.py
+++ myqiwi/request.py
@@ -2,7 +2,6 @@ import requests
 
 from myqiwi import exceptions
 
-warnings = True
 API_URl = "https://edge.qiwi.com/"
 
 headers = {}
@@ -13,15 +12,14 @@ def send(path, params=None, method="get", json=None):
     url = API_URl + path
     response = requests.request(method.upper(), url, params=params, json=json, headers=headers, proxies=proxy)
 
-    if warnings:
-        error_text = response.text
-        if 400 == response.status_code:
-            raise exceptions.ArgumentError(error_text)
-        if 401 == response.status_code:
-            raise exceptions.InvalidToken("Invalid token")
-        if 403 == response.status_code:
-            raise exceptions.NotHaveEnoughPermissions(error_text)
-        if 404 == response.status_code:
-            raise exceptions.NoTransaction(error_text)
+    error_text = response.text
+    if 400 == response.status_code:
+        raise exceptions.ArgumentError(error_text)
+    if 401 == response.status_code:
+        raise exceptions.InvalidToken("Invalid token")
+    if 403 == response.status_code:
+        raise exceptions.NotHaveEnoughPermissions(error_text)
+    if 404 == response.status_code:
+        raise exceptions.NoTransaction(error_text)
 
     return response.json()
